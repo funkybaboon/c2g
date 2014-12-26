7254629cd659ea58abf40fdf6be4154532d5dbd2
funkybaboon <funkybaboon@gmail.com>
2014-12-26 19:09:41 +0100

    added stadtmobil page

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp',['ngRoute', 'angularMoment']);
 
 myApp.controller('c2gController', ['$scope', function($scope) {
 
@@ -151,71 +151,172 @@ myApp.controller('c2gbController', ['$scope', function($scope) {
 }]);
 
 myApp.controller('smController', ['$scope', function($scope) {
-    $scope.vendor = "Stadtmobil";
     $scope.distance = 10;
-    $scope.time = 20;
-    $scope.airport = false;
+    $scope.time_hours = 20;
+    $scope.time_days = 0;
+    $scope.time_weeks = 0;
+    $scope.rate = 'A';
+    $scope.tariff = 'classic';
 
-    $scope.fee_day = 89;
-    $scope.fee_hour = 14.9;
-
-    $scope.fee_additionalkm = 0.29;
-
-    $scope.getDays = function( minutes ) {
-        return Math.floor(minutes / 1440);
-    };
-
-    $scope.getHours = function( minutes ) {
-        return Math.ceil((minutes - ($scope.getDays( minutes ) * 1440))/ 60);
-    };
-
-    $scope.getFreeKm = function( minutes ) {
-        var freeKm = 50;
-        if( $scope.getDays( minutes) > 0 ) {
-            freeKm = $scope.getDays( minutes ) * 200;
+    var stadtmobil_rates = {
+        classic: {
+            A: {
+                night: 0,
+                hour: 1.4,
+                day: 21,
+                week: 125,
+                km000: 0.2,
+                km101: 0.18,
+                km701: 0.18
+            },
+            B: {
+                night: 0,
+                hour: 2.2,
+                day: 25,
+                week: 140,
+                km000: 0.22,
+                km101: 0.19,
+                km701: 0.16
+            },
+            C: {
+                night: 0,
+                hour: 2.8,
+                day: 32,
+                week: 160,
+                km000: 0.26,
+                km101: 0.21,
+                km701: 0.17
+            },
+            D: {
+                night: 1,
+                hour: 3.2,
+                day: 35,
+                week: 190,
+                km000: 0.29,
+                km101: 0.25,
+                km701: 0.25
+            },
+            F: {
+                night: 2,
+                hour: 4.2,
+                day: 44,
+                week: 245,
+                km000: 0.33,
+                km101: 0.27,
+                km701: 0.27
+            }
+        },
+        basic: {
+            A: {
+                night: 0,
+                hour: 1.6,
+                day: 26,
+                week: 140,
+                km000: 0.22,
+                km101: 0.18,
+                km701: 0.18
+            },
+            B: {
+                night: 0,
+                hour: 2.7,
+                day: 30,
+                week: 155,
+                km000: 0.27,
+                km101: 0.21,
+                km701: 0.16
+            },
+            C: {
+                night: 0,
+                hour: 3.3,
+                day: 37,
+                week: 175,
+                km000: 0.31,
+                km101: 0.23,
+                km701: 0.17
+            },
+            D: {
+                night: 1,
+                hour: 3.7,
+                day: 40,
+                week: 210,
+                km000: 0.34,
+                km101: 0.25,
+                km701: 0.25
+            },
+            F: {
+                night: 2,
+                hour: 4.7,
+                day: 49,
+                week: 260,
+                km000: 0.38,
+                km101: 0.27,
+                km701: 0.27
+            }
+        },
+        business: {
+            A: {},
+            B: {},
+            C: {},
+            D: {},
+            F: {}
         }
-        return freeKm;
     };
 
-    $scope.getAdditionalKm = function( km, minutes ) {
-        var AdditionalKm = 0;
-        var freeKm = $scope.getFreeKm( minutes );
-        if( km - freeKm > 0 ) {
-            AdditionalKm = km - freeKm;
-        }
-        return AdditionalKm;
+    var getDuration = function( hours, days, weeks){
+        return moment.duration({
+            hours: hours,
+            days: days,
+            weeks: weeks
+        });
     };
 
-    $scope.getFee_additionalKm = function( km, minutes ) {
-        return $scope.getAdditionalKm( km, minutes ) * $scope.fee_additionalkm;
+    var getFeeTime = function( duration, rate ){
+        return (
+            duration.hours() * rate.hour +
+            duration.days() % 7 * rate.day +
+            Math.floor(duration.days() / 7) * rate.week
+            );
     };
 
-    $scope.getFeeDays = function( minutes ) {
-        return Math.floor(minutes / 1440) * $scope.fee_day;
-    };
-
-    $scope.getFeeHours = function( minutes ) {
-        return Math.ceil((minutes - ($scope.getDays( minutes ) * 1440))/ 60) * $scope.fee_hour;
-    };
-
-    $scope.getFeeAirport = function( airport ) {
-        fee = 0;
-        if( airport ){
-            fee = 4.90;
+    var getFeeDistance = function( km, rate){
+        var fee = 0;
+        if ( km >= 701 ) {
+            fee = 100 * rate.km000 + 600 * rate.km101 + (km-700) * rate.km701;
+        } else if ( km < 701 && km >= 101) {
+            fee = 100 * rate.km000 + (km-100) * rate.km101;
+        } else {
+            fee = km * rate.km000;
         }
         return fee;
     };
 
-    $scope.price = function( km, minutes, time_standing, airport) {
-        return (
-            $scope.getFee_additionalKm( km, minutes ) +
-            $scope.getFeeDays( minutes ) +
-            $scope.getFeeHours( minutes ) +
-            $scope.getFeeAirport( airport )
-            );
+    var getCurrentRate = function( rate, tariff) {
+        // studi and classic have the same rates
+        if( tariff === 'studi') {
+            tariff = 'classic';
+        }
+        return stadtmobil_rates[tariff][rate];
     };
 
 
+    var priceDistance = function( km, rate, tariff) {
+        var current_rate = getCurrentRate( rate, tariff );
+        return getFeeDistance(km, current_rate);
+    };
+
+    var priceTime = function( time_hours, time_days, time_weeks, rate, tariff) {
+        var current_rate = getCurrentRate( rate, tariff );
+        var duration = getDuration( time_hours, time_days, time_weeks);
+        return getFeeTime(duration, current_rate);
+    };
+
+    var price = function( distance, time_hours, time_days, time_weeks, rate, tariff) {
+        return priceDistance( distance, rate, tariff) + priceTime( time_hours, time_days, time_weeks, rate, tariff);
+    };
+
+    $scope.price = price;
+    $scope.priceDistance = priceDistance;
+    $scope.priceTime = priceTime;
 
 }]);
 
@@ -229,6 +330,9 @@ myApp.controller("TabController", function($scope) {
   $scope.setTab = function(setTab) {
       $scope.tab = setTab;
   };
+});
+
+myApp.controller("RateController", function($scope) {
 });
 
 myApp.config(['$routeProvider',
@@ -245,10 +349,6 @@ myApp.config(['$routeProvider',
     when('/sm', {
         templateUrl: 'partials/sm.html',
         controller: 'smController'
-    }).
-    when('/gemstore', {
-        templateUrl: 'partials/gemstore.html',
-        controller: 'StoreController'
     }).
     otherwise({
         redirectTo: '/c2g'
